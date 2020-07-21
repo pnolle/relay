442d4db01b19ed10c5e6d88fe4fd8afd1922e98c
Nik <pnolle@web.de>
2020-07-21 20:45:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# relay
+
+Testing Teensy LC with a Saintsmart 8-Channel 5V 2A Solid State Relay
---
+++ Relaytest/Relaytest.ino
@@ -0,0 +1,44 @@
+const int relaisPin1 = 1;
+const int relaisPin2 = 2;
+const int relaisPin3 = 3;
+const int relaisPin4 = 4;
+ 
+void setup() {
+  pinMode(relaisPin1, OUTPUT);
+  pinMode(relaisPin2, OUTPUT);
+  pinMode(relaisPin3, OUTPUT);
+  pinMode(relaisPin4, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(relaisPin1, HIGH);
+  digitalWrite(relaisPin2, LOW);
+  digitalWrite(relaisPin3, HIGH);
+  digitalWrite(relaisPin4, LOW);
+  delay(3000);
+  digitalWrite(relaisPin1, LOW);
+  digitalWrite(relaisPin2, HIGH);
+  digitalWrite(relaisPin3, LOW);
+  digitalWrite(relaisPin4, HIGH);
+  delay(500);
+  digitalWrite(relaisPin1, HIGH);
+  digitalWrite(relaisPin2, LOW);
+  digitalWrite(relaisPin3, LOW);
+  digitalWrite(relaisPin4, LOW);
+  delay(500);
+  digitalWrite(relaisPin1, LOW);
+  digitalWrite(relaisPin2, HIGH);
+  digitalWrite(relaisPin3, LOW);
+  digitalWrite(relaisPin4, LOW);
+  delay(500);
+  digitalWrite(relaisPin1, LOW);
+  digitalWrite(relaisPin2, LOW);
+  digitalWrite(relaisPin3, HIGH);
+  digitalWrite(relaisPin4, LOW);
+  delay(500);
+  digitalWrite(relaisPin1, LOW);
+  digitalWrite(relaisPin2, LOW);
+  digitalWrite(relaisPin3, LOW);
+  digitalWrite(relaisPin4, HIGH);
+  delay(500);
+}
